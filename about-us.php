7a055638aacbbffb2d3d1ad90f84bd862464e450
ian_ndiga <ianndiga0@gmail.com>
2024-09-17 08:34:33 +0300

    online shopping

---
+++ about-us.php
@@ -0,0 +1,54 @@
+<?php session_start();
+include_once('includes/config.php');
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Urban Footwear | Category wise Shop </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+       <!--  <link href="css/bootstrap.min.css" rel="stylesheet" /> -->
+    </head>
+    <body>
+<?php include_once('includes/header.php');?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">About Us</h1>
+        
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+E-commerce is revolutionizing the way we all shop in Kenya. Why do you want to hop from one store to another in search of the latest products when you can find it on the Internet in a single click? Not only Footwear. Urban Footwear is your best bet. Shop in your PJs, at night or in the wee hours of the morning. This e-commerce never shuts down.
+<br />What's more, with our year-round shopping festivals and events, our prices are irresistible. We're sure you'll find yourself picking up more than what you had in mind. If you are wondering why you should shop from Urban Footwear when there are multiple options available to you, well, the below will answer your question.
+
+                </div>
+            </div>
+
+ 
+</div>
+        </section>
+        <!-- Footer-->
+   <?php include_once('includes/footer.php'); ?>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
